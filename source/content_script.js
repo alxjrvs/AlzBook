ef1cc20f9f7d3e1830187cd653988881d14e8b67
Alex Jarvis <alxjrvs@gmail.com>
2016-06-04 15:25:21 -0400

    Overlay button created

--- source/content_script.js
+++ source/content_script.js
@@ -1,1 +1,10 @@
-alert("IT WORKS");
+function addOverlay() {
+  btn = document.createElement('a')
+  btn.text = "#EndAlz"
+  btn.href = "http://act.alz.org/site/TR?fr_id=8480&pg=entry"
+  btn.id = 'end-alz-btn'
+
+  document.body.appendChild(btn) 
+}
+
+addOverlay();
